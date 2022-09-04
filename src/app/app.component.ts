0727c2867e512c245e4d5c147c11bb2abb4cf51a
Cecilia von Wachenfeldt <cecwac@gmail.com>
2022-09-04 15:41:13 +0200

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-quickstart';
+  milliseconds: number = 10;
+
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi',
+    'Episode IX – The Rise of Skywalker',
+  ];
+
+  // drop(event: CdkDragDrop<string[]>) {
+  //   moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  // }
+
+  onDragExit(): void {
+    console.log("sorted", this.milliseconds);
+    navigator.vibrate(this.milliseconds);
+  }
+
+  logTouchStart(event: any) {
+    console.log("logTouch", this.milliseconds);
+  }
+
 }
